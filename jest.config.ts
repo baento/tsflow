1e56852fe91bb7876592fe3658d12ecdc2da6f53
Mathis Engels <mathisengels@yahoo.fr>
2024-06-04 00:00:22 +0200

    ci: setup tooling and configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
-import { Config } from "jest";
+import type { Config } from "jest";
 
 export default {
   preset: "ts-jest",
   testMatch: ["<rootDir>/src/**/*.test.[jt]s"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
 } satisfies Config;
